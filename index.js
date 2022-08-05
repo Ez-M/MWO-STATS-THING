6078faa489c795a439437356b67141252eef03eb
Ezequiel <martinez.ezequiel42@gmail.com>
2022-08-05 12:16:21 -0700

    getData now actually pushes the results of the API call back into the .then chain

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import fs from 'fs';
+import fs from "fs";
 
 // const https = require("https");
 
@@ -13,15 +13,12 @@ import fs from 'fs';
 //         // console.log(JSON.parse(data));
 //         data = JSON.parse(data);
 
-
-
 //         console.log("getdata done");
-
 
 //         // console.log(firstObj)
 //         return data;
 //       });
-//     });    
+//     });
 // };
 
 /* 
@@ -44,72 +41,84 @@ import fs from 'fs';
 }
 */
 
-
-
-
 // const url = "https://mwomercs.com/api/v1/matches/160474522304217?api_token=Gbz4lg4OIZcssVmqNove98pQVHnzRKctUZpsTZIx5xGQpWQ7eL0n8GdxBaUl";
 
+// let url = `https://mwomercs.com/api/v1/matches/${matchID}?api_token=Gbz4lg4OIZcssVmqNove98pQVHnzRKctUZpsTZIx5xGQpWQ7eL0n8GdxBaUl`;
 
-
-function mainFunc(matchIDin, unitTagIn){
-  
+function mainFunc(matchIDin, unitTagIn) {
   let desiredTag = unitTagIn;
-  let  matchID = matchIDin;
-  let url = `https://mwomercs.com/api/v1/matches/${matchID}?api_token=Gbz4lg4OIZcssVmqNove98pQVHnzRKctUZpsTZIx5xGQpWQ7eL0n8GdxBaUl`;
+  let matchID = matchIDin;
+  const url = "https://mwomercs.com/api/v1/matches/160474522304217?api_token=Gbz4lg4OIZcssVmqNove98pQVHnzRKctUZpsTZIx5xGQpWQ7eL0n8GdxBaUl";
 
-    let oldData = loadRoster();   //pull exising pilot records from saved file
-    // console.log(oldData)  //sanity check
+  let oldData = loadRoster(); //pull exising pilot records from saved file
+  console.log("oldData pulled" + oldData); //sanity check
 
-    let newData = getData(url); //pull new data from API with matchID
+  getData(url).then(res => console.log(res))
+ 
+  
+  // 
+  /* 
+  .then(res => res.json())
+  .then(data => {
+    let newData = data;
+console.log(newData);
 
-    let finishedData = mergeRecords(oldData, newData, desiredTag);  //process the new data into the old data
+    let finishedData = mergeRecords(oldData, newData, desiredTag); //process the new data into the old data
+  console.log("finishedData pulled " + finishedData);
 
-    saveRecords(finishedData);  //save the processed data
+  saveRecords(finishedData); //save the processed data
+  console.log("finishedData saved");
+  });  //pull new data from API with matchID  
+
+  
+  */
 }
 
-
 const getData = async (url) => {
-
-    const newData = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'Accept': 'Application/json'
-      }
-    })
+  const newData = await fetch(url, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      Accept: "Application/json",
+    },
+  })
     // .then(res => res.json());
-    .then((response)=> {
+     .then((response) => {
+      let potato = response.json();
+      
 
-      let potato = response.json()
+      return potato;
+    }).then((data => {
+      // console.log(data)      
 
-     return potato;
-
-    })
-    .then((data) =>{
-    //   console.log(data)
-      // trimData(data);
-        })
-    // console.log(newData);
-    // setReturnedData(newData);
-
-  };
-
-
-  function mergeRecords(oldDataIn, newDataIn, desiredTagIn) {
-    for(var X in newDataIn.UserDetails) {
-        let currentBoi = newDataIn.UserDetails[X];   // select the input data we're checking for
-        if(desiredTagIn !== undefined ? currentBoi.UnitTag == toString(desiredTagIn) : true) {      //check first if a valid unit tag has been entered, otherwise simply run all pilots
-            // console.log(currentBoi);          
- 
-    let nameCheck = 0;
+      return data      
+    }))
     
+    return newData;  
+};
 
+function mergeRecords(oldDataIn, newDataIn, desiredTagIn) {
 
-      for (var Y in oldDataIn.Pilots) {
-        let targetBoi = oldDataIn.Pilots[Y];  // select the saved data we're checking against
+  let workingData = oldDataIn; 
+  console.log(newDataIn.UserDetails);
 
+  for (var X in newDataIn.UserDetails) {
+    let currentBoi = newDataIn.UserDetails[X]; // select the input data we're checking for
+    if (  true
+      /* desiredTagIn !== undefined
+        ? currentBoi.UnitTag == toString(desiredTagIn)
+        : true */
+    ) {
+      //check first if a valid unit tag has been entered, otherwise simply run all pilots
+      // console.log(currentBoi);
 
-        if (targetBoi.Username.indexOf(currentBoi.Username) !== -1 ){   //checking if the inputData matches any names in the saved data's username array. This will allow for potentially merging new and old account names
+      let nameCheck = -1;
+
+      for (var Y in workingData.Pilots) {
+        let targetBoi = workingData.Pilots[Y]; // select the saved data we're checking against
+
+        if (targetBoi.Username.indexOf(currentBoi.Username) !== -1) {
+          //checking if the inputData matches any names in the saved data's username array. This will allow for potentially merging new and old account names
           targetBoi.MechItemID.push(currentBoi.MechItemID);
           targetBoi.MechName.push(currentBoi.MechName);
           targetBoi.SkillTier.push(currentBoi.SkillTier);
@@ -123,70 +132,52 @@ const getData = async (url) => {
           targetBoi.TeamDamage.push(currentBoi.TeamDamage);
           targetBoi.matchIDs.push(matchID);
         } else {
-          
-          nameCheck++
+          nameCheck++;
 
-           if (nameCheck > oldDataIn.Pilots.length) {
-          let newPilot = {
-            
-        "Username": [currentBoi.Username],  
-        "MechItemID": [currentBoi.MechItemID],
-        "MechName": [currentBoi.MechName],
-        "SkillTier": [currentBoi.SkillTier],
-        "HealthPercentage": [currentBoi.HealthPercentage],
-        "Kills": [currentBoi.Kills],
-        "KillsMostDamage": [currentBoi.KillsMostDamage],
-        "Assists": [currentBoi.Assists],
-        "ComponentsDestroyed": [currentBoi.ComponentsDestroyed],
-        "MatchScore": [currentBoi.MatchScore],
-        "Damage": [currentBoi.Damage],
-        "TeamDamage": [currentBoi.TeamDamage],
-        "matchIDs": [matchID]
-              }          
-              oldDataIn.Pilots.push(newPilot)
+          if (nameCheck > workingData.Pilots.length) {
+            let newPilot = {
+              Username: [currentBoi.Username],
+              MechItemID: [currentBoi.MechItemID],
+              MechName: [currentBoi.MechName],
+              SkillTier: [currentBoi.SkillTier],
+              HealthPercentage: [currentBoi.HealthPercentage],
+              Kills: [currentBoi.Kills],
+              KillsMostDamage: [currentBoi.KillsMostDamage],
+              Assists: [currentBoi.Assists],
+              ComponentsDestroyed: [currentBoi.ComponentsDestroyed],
+              MatchScore: [currentBoi.MatchScore],
+              Damage: [currentBoi.Damage],
+              TeamDamage: [currentBoi.TeamDamage],
+              matchIDs: [matchID],
+            };
+            workingData.Pilots.push(newPilot);
+          }
         }
-        
-        }
-        
-       
       }
-    
+    }
   }
 
-        }
-      
+  return workingData;
+}
 
+function loadRoster() {
+  let rawData = fs.readFileSync("./teamData.json");
+  return JSON.parse(rawData);
+}
 
-
-
-        }
-        
-       
-  
-
-  function loadRoster(){
-    let rawData = fs.readFileSync('./teamData.json');
-     return JSON.parse(rawData);
+function saveRecords(finishedDataIn) {
+  for (var x in finishedDataIn.Pilots){
+    // console.log(finishedDataIn.Pilots[x]);
   }
+  // console.log(finishedDataIn);
+}
 
+const matchIDTest = 160474522304217;
+const unitTagTest = "AW";
 
-  function saveRecords(finishedDataIn) {
+mainFunc(matchIDTest, unitTagTest);
 
-    console.log(finishedDataIn);
-
-  }
-
-  const matchIDTest = 160474522304217;
-  const unitTagTest = "AW"
-
-  mainFunc(matchIDTest, unitTagTest);
-
-
-
- 
-
-
-  /* 
+/* 
     {    
     "Pilots":   [
         Username: ['ArkonEnt'],  
@@ -206,10 +197,7 @@ const getData = async (url) => {
 }
 */
 
-
-
-
- /* 
+/* 
     let nameCheck = 0;
     
   for x in dataIn.UserDetails {
