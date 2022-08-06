13828d47df8fae80c8919ddeaefa8e1cecfbca04
Ezequiel <martinez.ezequiel42@gmail.com>
2022-08-06 14:19:41 -0700

    Added beginning of console UI

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import fs from "fs";
 import inquirer from "inquirer";
 import { match } from "assert";
-
+import path from "path";
 
 // const https = require("https");
 
@@ -48,58 +48,40 @@ import { match } from "assert";
 
 // let url = `https://mwomercs.com/api/v1/matches/${matchID}?api_token=Gbz4lg4OIZcssVmqNove98pQVHnzRKctUZpsTZIx5xGQpWQ7eL0n8GdxBaUl`;
 
-
-
 async function mainFunc(matchIDin, unitTagIn) {
   let desiredTag = unitTagIn;
-  let numOID = matchIDin.length
-  console.log(matchIDin.length)
+  let numOID = matchIDin.length;
+  console.log(matchIDin.length);
 
   let oldData = loadRoster(); //pull exising pilot records from saved file
   console.log("oldData pulled" + oldData); //sanity check
 
-  
-  for(let i = 0; i < matchIDin.length; i++){
+  for (let i = 0; i < matchIDin.length; i++) {
+    let matchID = matchIDin[i];
 
-    let matchID = matchIDin[i]
-   
-  let url = `https://mwomercs.com/api/v1/matches/${matchID}?api_token=Gbz4lg4OIZcssVmqNove98pQVHnzRKctUZpsTZIx5xGQpWQ7eL0n8GdxBaUl`;
+    let url = `https://mwomercs.com/api/v1/matches/${matchID}?api_token=Gbz4lg4OIZcssVmqNove98pQVHnzRKctUZpsTZIx5xGQpWQ7eL0n8GdxBaUl`;
 
-  
-  
+    await getData(url)
+      .then((res) => {
+        //pull new data from API with matchID
 
-   await getData(url).then((res) => {      //pull new data from API with matchID    
-    
-    
-      let newData = res;
-  
-      let finishedData = mergeRecords(oldData, newData, desiredTag, matchID); //process the new data into the old data
-      oldData = finishedData;
-    console.log("finishedData pulled " );
+        let newData = res;
 
-    return finishedData;
-    
-    
-    }).then((res)=>{
+        let finishedData = mergeRecords(oldData, newData, desiredTag, matchID); //process the new data into the old data
+        oldData = finishedData;
+        console.log("finishedData pulled ");
 
-      if( i == matchIDin.length-1){
-        // console.log('done')
-      saveRecords(res); //save the processed data
-      console.log("finishedData saved");
-    }
-
-    });
-
-
+        return finishedData;
+      })
+      .then((res) => {
+        if (i == matchIDin.length - 1) {
+          // console.log('done')
+          saveRecords(res); //save the processed data
+          console.log("finishedData saved");
+        }
+      });
   }
-
-  
-  
-
 }
-
-
-
 
 const getData = async (url) => {
   const newData = await fetch(url, {
@@ -110,41 +92,41 @@ const getData = async (url) => {
     },
   })
     // .then(res => res.json());
-     .then((response) => {
+    .then((response) => {
       let potato = response.json();
-      
 
       return potato;
-    }).then((data => {
-      // console.log(data)      
+    })
+    .then((data) => {
+      // console.log(data)
 
-      return data      
-    }))
-    
-    return newData;  
+      return data;
+    });
+
+  return newData;
 };
 
 function mergeRecords(oldDataIn, newDataIn, desiredTagIn, matchIDin) {
+  let matchID = matchIDin;
 
-  let matchID = matchIDin
-
-  let workingData = oldDataIn; 
+  let workingData = oldDataIn;
 
   let filterTag = desiredTagIn;
-  if (typeof filterTag === 'undefined') {filterTag = ""};
+  if (typeof filterTag === "undefined") {
+    filterTag = "";
+  }
   // console.log(newDataIn.UserDetails);
 
   for (var X in newDataIn.UserDetails) {
     let currentBoi = newDataIn.UserDetails[X]; // select the input data we're checking for
-    
-    
-    if ( filterTag !== "" ? currentBoi.UnitTag == filterTag  : true ) {
+
+    if (filterTag !== "" ? currentBoi.UnitTag == filterTag : true) {
       //check first if a valid unit tag has been entered, otherwise simply run all pilots
       // console.log(currentBoi);
 
       let nameCheck = 0;
 
-      if(workingData.Pilots.length == 0){
+      if (workingData.Pilots.length == 0) {
         let newPilot = {
           Username: [currentBoi.Username],
           MechItemID: [currentBoi.MechItemID],
@@ -161,57 +143,51 @@ function mergeRecords(oldDataIn, newDataIn, desiredTagIn, matchIDin) {
           MatchIDs: [matchID],
         };
         workingData.Pilots.push(newPilot);
-      } else{
+      } else {
+        for (var Y in workingData.Pilots) {
+          let targetBoi = workingData.Pilots[Y]; // select the saved data we're checking against
 
-              for (var Y in workingData.Pilots) {
-        let targetBoi = workingData.Pilots[Y]; // select the saved data we're checking against
-        
-        
+          if (targetBoi.Username.indexOf(currentBoi.Username) !== -1) {
+            if (targetBoi.MatchIDs.indexOf(matchID) === -1) {
+              //checking if the inputData matches any names in the saved data's username array. This will allow for potentially merging new and old account names
+              targetBoi.MechItemID.push(currentBoi.MechItemID);
+              targetBoi.MechName.push(currentBoi.MechName);
+              targetBoi.SkillTier.push(currentBoi.SkillTier);
+              targetBoi.HealthPercentage.push(currentBoi.HealthPercentage);
+              targetBoi.Kills.push(currentBoi.Kills);
+              targetBoi.KillsMostDamage.push(currentBoi.KillsMostDamage);
+              targetBoi.Assists.push(currentBoi.Assists);
+              targetBoi.ComponentsDestroyed.push(
+                currentBoi.ComponentsDestroyed
+              );
+              targetBoi.MatchScore.push(currentBoi.MatchScore);
+              targetBoi.Damage.push(currentBoi.Damage);
+              targetBoi.TeamDamage.push(currentBoi.TeamDamage);
+              targetBoi.MatchIDs.push(matchID);
+            }
+          } else {
+            nameCheck++;
 
-        if (targetBoi.Username.indexOf(currentBoi.Username) !== -1 ) {
-
-          if (targetBoi.MatchIDs.indexOf(matchID) === -1) {
-          //checking if the inputData matches any names in the saved data's username array. This will allow for potentially merging new and old account names
-          targetBoi.MechItemID.push(currentBoi.MechItemID);
-          targetBoi.MechName.push(currentBoi.MechName);
-          targetBoi.SkillTier.push(currentBoi.SkillTier);
-          targetBoi.HealthPercentage.push(currentBoi.HealthPercentage);
-          targetBoi.Kills.push(currentBoi.Kills);
-          targetBoi.KillsMostDamage.push(currentBoi.KillsMostDamage);
-          targetBoi.Assists.push(currentBoi.Assists);
-          targetBoi.ComponentsDestroyed.push(currentBoi.ComponentsDestroyed);
-          targetBoi.MatchScore.push(currentBoi.MatchScore);
-          targetBoi.Damage.push(currentBoi.Damage);
-          targetBoi.TeamDamage.push(currentBoi.TeamDamage);
-          targetBoi.MatchIDs.push(matchID);
-        }
-
-        } else {
-          nameCheck++;
-
-          if (nameCheck >= workingData.Pilots.length) {
-            let newPilot = {
-              Username: [currentBoi.Username],
-              MechItemID: [currentBoi.MechItemID],
-              MechName: [currentBoi.MechName],
-              SkillTier: [currentBoi.SkillTier],
-              HealthPercentage: [currentBoi.HealthPercentage],
-              Kills: [currentBoi.Kills],
-              KillsMostDamage: [currentBoi.KillsMostDamage],
-              Assists: [currentBoi.Assists],
-              ComponentsDestroyed: [currentBoi.ComponentsDestroyed],
-              MatchScore: [currentBoi.MatchScore],
-              Damage: [currentBoi.Damage],
-              TeamDamage: [currentBoi.TeamDamage],
-              MatchIDs: [matchID],
-            };
-            workingData.Pilots.push(newPilot);
+            if (nameCheck >= workingData.Pilots.length) {
+              let newPilot = {
+                Username: [currentBoi.Username],
+                MechItemID: [currentBoi.MechItemID],
+                MechName: [currentBoi.MechName],
+                SkillTier: [currentBoi.SkillTier],
+                HealthPercentage: [currentBoi.HealthPercentage],
+                Kills: [currentBoi.Kills],
+                KillsMostDamage: [currentBoi.KillsMostDamage],
+                Assists: [currentBoi.Assists],
+                ComponentsDestroyed: [currentBoi.ComponentsDestroyed],
+                MatchScore: [currentBoi.MatchScore],
+                Damage: [currentBoi.Damage],
+                TeamDamage: [currentBoi.TeamDamage],
+                MatchIDs: [matchID],
+              };
+              workingData.Pilots.push(newPilot);
+            }
           }
         }
-
-      }
-
-
       }
     }
   }
@@ -225,7 +201,7 @@ function loadRoster() {
 }
 
 function saveRecords(finishedDataIn) {
-  for (var x in finishedDataIn.Pilots){
+  for (var x in finishedDataIn.Pilots) {
     console.log(finishedDataIn.Pilots[x]);
   }
 
@@ -233,53 +209,55 @@ function saveRecords(finishedDataIn) {
   // console.log(finishedDataIn);
 }
 
-
 function getRosters() {
   // try{
-    if (fs.existsSync("./rosters") == true) {
-    console.log("director exists.");
+  if (fs.existsSync("./rosters") == true) {
+    console.log("directory exists.");
   } else {
-    
-    fs.mkdir('./rosters', (err) => {  if (err) return err;})
+    fs.mkdir("./rosters", (err) => {
+      if (err) {return err};
+    });
     console.log("Roster directory did not exist, creating roster folder");
-
   }
-// } catch(e) {
-//   console.log("An error occured.");
-// }
+
+  
+
+  const rostersList = fs.readdirSync("./rosters");
+
+  rostersList.push('Make New Roster');
+
+  return rostersList;
+
+  
+  // } catch(e) {
+  //   console.log("An error occured.");
+  // }
 }
 
-
-
-
-
-const matchIDTest = [152567485709779,
-  152352737344201,
-  152889608258195,
-  152515946102057,
-  152696334729162];
+const matchIDTest = [
+  152567485709779, 152352737344201, 152889608258195, 152515946102057,
+  152696334729162,
+];
 
 const unitTagTest = "AW";
 
 const testText = "sss";
 
- getRosters();
+const rostersList = getRosters();
 
-inquirer.prompt([
-  {
-    type: "list",
-    name: "targetFile",
-    message: "Which roster would you like to use?",
-    choices: matchIDTest
-
-  }
-
-]).then((data) => {
-  console.log(data.targetFile)
-  mainFunc(matchIDTest, unitTagTest);
-})
-
-
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "targetFile",
+      message: "Which roster would you like to use?",
+      choices: rostersList,
+    },
+  ])
+  .then((data) => {
+    console.log(data.targetFile);
+    mainFunc(matchIDTest, unitTagTest);
+  });
 
 /* 
     {    
@@ -357,10 +335,7 @@ inquirer.prompt([
   }
   */
 
-
-
-
-  /* 
+/* 
 
 160474522304217
 
